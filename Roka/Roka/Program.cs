b620c6a13effe3b1d302d276dee54102b7bbfe52
SagiDaniel <sagidaniel24@gmail.com>
2022-10-11 15:41:00 +0200

    mezo szelesseg, magassag bekerdezes, fu tanla elkezdese

---
+++ Roka/Roka/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Roka
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg hogy milyen széles legyen a mező:");
+            int mezox = int.Parse(Console.ReadLine());
+            Console.Write("Adja meg hogy milyen magas legyen a mező:");
+            int mezoy = int.Parse(Console.ReadLine());
+            int ossz = mezoy * mezox;
+            Console.WriteLine($"Összesen {ossz} mező van");
+            Random rnd = new Random();
+            int[,] fu= new int[mezox, mezoy];
+
+
+            Console.ReadKey();
+        }
+    }
+}
